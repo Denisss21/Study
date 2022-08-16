99eea94822b7581ab342f725fb830a5240e91368
Denisss21 <d.kalnik21@gmail.com>
2022-08-16 16:35:51 +0300

    new_commit

---
+++ test_denys/target/debug/deps/test_denys-25fc845668306e01.d
@@ -0,0 +1,5 @@
+/home/denys/Study/Study/test_denys/target/debug/deps/test_denys-25fc845668306e01.rmeta: src/main.rs
+
+/home/denys/Study/Study/test_denys/target/debug/deps/test_denys-25fc845668306e01.d: src/main.rs
+
+src/main.rs:
---
+++ test_denys/target/debug/deps/test_denys-f085f785230b6d04.d
@@ -0,0 +1,5 @@
+/home/denys/Study/Study/test_denys/target/debug/deps/test_denys-f085f785230b6d04.rmeta: src/main.rs
+
+/home/denys/Study/Study/test_denys/target/debug/deps/test_denys-f085f785230b6d04.d: src/main.rs
+
+src/main.rs:
